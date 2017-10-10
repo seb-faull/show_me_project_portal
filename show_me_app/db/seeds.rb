d1e435d9da2b47eacec69ba1259a6cfef53938fc
sbaxter95 <sbaxter95@outlook.com>
2017-10-10 19:45:21 +0100

    added second project

--- show_me_app/db/seeds.rb
+++ show_me_app/db/seeds.rb
@@ -15,7 +15,7 @@ u1 = User.create!(email: "sks@outlook.com", password: "123456", name: "Bob", yea
 u2 = User.create!(email: "ea@hotmail.com", password: "123456", name: "John", year: "2015"), role: "Trainer")
 
 Project.create!(title: "Project 1", description: "Game built with HTML/CSS, JavaScript and jQuery", live: false, user_id: u1.id)
-Project.create!(title: "Project 2", description: "Game built with HTML/CSS, JavaScript and jQuery", live: false, user_id: u1.id)
+Project.create!(title: "Project 2", description: "Web Application built with Ruby on Rails ", live: false, user_id: u2.id)
 
 
 
