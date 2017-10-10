77f6b5c37b804f99719d7ba35ff4d54e972eda4e
seb-faull <seb_faull@hotmail.co.uk>
2017-10-10 12:02:11 +0100

    Without loggin in you can now see all projects

--- show_me_app/app/controllers/application_controller.rb
+++ show_me_app/app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+	protect_from_forgery with: :exception
+	
+
+
+	# authenticated :user do
+	# 	root to: "projects#index", as: :authenticated_root
+	# end
+
+	# authenticated :user do
+	#   root to: "projects#index", as: :authenticated_root
+	# end
+
+	# unauthenticated do
+	#   root to: "static#home"
+	# end
+
 end
--- show_me_app/app/controllers/projects_controller.rb
+++ show_me_app/app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+  before_action :authenticate_user!
+
   #Index
   def index
     @projects = current_user.projects
