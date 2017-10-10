2f3d12089ccb88ca4c46a066ba4abd6fa19d1617
seb-faull <seb_faull@hotmail.co.uk>
2017-10-10 10:46:46 +0100

    Generated project controller

--- show_me_app/config/routes.rb
+++ show_me_app/config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'projects/index'
+
+  get 'projects/show'
+
+  get 'projects/new'
+
+  get 'projects/create'
+
+  get 'projects/edit'
+
+  get 'projects/update'
+
+  get 'projects/destroy'
+
 	resources :projects
 
 	root "projects#index"
