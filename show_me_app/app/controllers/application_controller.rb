23088adef0936eed2973d235470edf6042c47304
stannard95 <14066588@brookes.ac.uk>
2017-10-10 15:06:29 +0100

    field for role year and edit profile

--- show_me_app/app/controllers/application_controller.rb
+++ show_me_app/app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 
+	#NEW STUFF
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+        	
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :role, :year, :password])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :role, :year, :password, :current_password])
+        end
 	# authenticated :user do
 	# 	root to: "projects#index", as: :authenticated_root
 	# end
