727e1aedf91e42aa87544831e1bcad161addccb9
sbaxter95 <sbaxter95@outlook.com>
2017-10-10 19:44:12 +0100

    created first project

--- show_me_app/db/seeds.rb
+++ show_me_app/db/seeds.rb
@@ -14,6 +14,9 @@
 u1 = User.create!(email: "sks@outlook.com", password: "123456", name: "Bob", year: "2017", role: "Spartan")
 u2 = User.create!(email: "ea@hotmail.com", password: "123456", name: "John", year: "2015"), role: "Trainer")
 
+Project.create!(title: "Project 1", description: "Game built with HTML/CSS, JavaScript and jQuery", live: false, user_id: u1.id)
+Project.create!(title: "Project 2", description: "Game built with HTML/CSS, JavaScript and jQuery", live: false, user_id: u1.id)
+
 
 
   # create_table "projects", force: :cascade do |t|
