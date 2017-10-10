bb3751b82de3d9aa8dc02b7b45e7242ef0bb53b5
seb-faull <seb_faull@hotmail.co.uk>
2017-10-10 10:52:56 +0100

    Added comments for set up for project controller

--- show_me_app/app/controllers/projects_controller.rb
+++ show_me_app/app/controllers/projects_controller.rb
@@ -1,22 +1,38 @@
 class ProjectsController < ApplicationController
+
+  #Index
   def index
   end
 
+
+  #Show
   def show
   end
 
+
+  #New
   def new
   end
 
+
+  #Create
   def create
   end
 
+
+  #Edit
   def edit
   end
 
+
+  #Update
   def update
   end
 
+
+  #Destroy
   def destroy
   end
+
+  
 end
--- show_me_app/config/routes.rb
+++ show_me_app/config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'projects/index'
-
-  get 'projects/show'
-
-  get 'projects/new'
-
-  get 'projects/create'
-
-  get 'projects/edit'
-
-  get 'projects/update'
-
-  get 'projects/destroy'
-
 	resources :projects
 
 	root "projects#index"
