6e6fd49c1251ce0ae1b5948f7abb44683463da02
stannard95 <14066588@brookes.ac.uk>
2017-10-13 10:40:34 +0100

    added aws credientials

--- show_me_app/config/environments/production.rb
+++ show_me_app/config/environments/production.rb
@@ -4,10 +4,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    bucket: ENV.fetch('show-case'),
+    access_key_id: ENV.fetch('AWSAccessKeyId=AKIAIODROLQJHGYXZSRQ'),
+    secret_access_key: ENV.fetch('AWSSecretKey=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'),
+    s3_region: ENV.fetch('EU (London)'),
   }
   }
   # Settings specified here will take precedence over those in config/application.rb.
